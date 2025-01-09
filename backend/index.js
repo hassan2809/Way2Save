b75987668ce77b4eff38c7eeff4085d64bbdcc05
Hassan Mehmood <chaudahryhassan786@gmail.com>
2025-01-10 00:26:36 +0500

    .

--- backend/index.js
+++ backend/index.js
@@ -21,10 +21,10 @@ app.get('/', (req, res) => {
 app.use("/auth", AuthRouter)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, 'dist')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
   });
 }
 
